860bb1560bdd0247adf1bc53cac4f82c2ec5f33f
kornel <korneljordan1203@gmail.com>
2022-11-23 09:28:48 +0100

    fixed api url

--- src/app/services/book-http.service.ts
+++ src/app/services/book-http.service.ts
@@ -4,14 +4,14 @@ import { Observable } from 'rxjs';
 import { Book } from '../model/book.model';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root'
 })
 export class BookHttpService {
-  private BOOKS_API_URL: string = 'http://localhost:5501/books/';
+	private BOOKS_API_URL: string = 'http://localhost:5501/api/books/';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  public getAllBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.BOOKS_API_URL);
-  }
+	public getAllBooks(): Observable<Book[]> {
+		return this.http.get<Book[]>(this.BOOKS_API_URL);
+	}
 }
